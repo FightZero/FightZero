aa305f5441ee72974c9568dfe984a3f1a4a62e5f
teamclouday <teamclouday@gmail.com>
2021-11-13 12:23:06 -0500

    fix bug in scripts

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 # prepare game for the environment
 apt install p7zip-full wget
 pip install -r requirements.txt
-wget https://edgeemu.net/down.php?id=12765 -O game.7z
-7z x game.7z -o game
+wget https://edgeemu.net/down.php?id=12765 -nc -O game.7z
+7z x game.7z -o./game -y
 python -m retro.import game
