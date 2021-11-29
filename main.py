22d110107dc09788fb4c49aa72016be821ec0f29
teamclouday <teamclouday@gmail.com>
2021-11-29 11:02:15 -0500

    update main

--- main.py
+++ main.py
@@ -1,44 +1,41 @@
-import sys
-from time import sleep
+import argparse
 from py4j.java_gateway import JavaGateway, GatewayParameters, CallbackServerParameters, get_field
 from KickAI import KickAI
 from DisplayInfo import DisplayInfo
-def check_args(args):
-	for i in range(argc):
-		if args[i] == "-n" or args[i] == "--n" or args[i] == "--number":
-			global GAME_NUM
-			GAME_NUM = int(args[i+1])
 
-def start_game():
-        p1 = KickAI(gateway)
-        p2 = DisplayInfo(gateway)
-        manager.registerAI(p1.__class__.__name__, p1)
-        manager.registerAI(p2.__class__.__name__, p2)
-        print("Start game")
+def run(args, gateway):
+    p1 = KickAI(gateway)
+    p2 = DisplayInfo(gateway)
+    manager = gateway.entry_point
+    manager.registerAI(p1.__class__.__name__, p1)
+    manager.registerAI(p2.__class__.__name__, p2)
 
-        game = manager.createGame("ZEN", "ZEN",
+    game = manager.createGame("ZEN", "ZEN",
                                   p1.__class__.__name__,
                                   p2.__class__.__name__,
-                                  GAME_NUM)
-        manager.runGame(game)
+                                  args.number)
+    manager.runGame(game)
 
-        print("After game")
-        sys.stdout.flush()
+def connect(args):
+    gateway = JavaGateway(gateway_parameters=GatewayParameters(port=args.port), callback_server_parameters=CallbackServerParameters())
+    return gateway
 
-def close_gateway():
+def disconnect(gateway):
 	gateway.close_callback_server()
 	gateway.close()
 
-def main_process():
-	check_args(args)
-	start_game()
-	close_gateway()
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--number", type=int, help="Number of rounds to play", default=2)
+    parser.add_argument("-p", "--port", type=int, help="Game server port", default=4242)
 
-args = sys.argv
-argc = len(args)
-GAME_NUM = 2
-gateway = JavaGateway(gateway_parameters=GatewayParameters(port=4242), callback_server_parameters=CallbackServerParameters());
-manager = gateway.entry_point
+    args = parser.parse_args()
 
-main_process()
+    gateway = connect(args)
 
+    try:
+        run(args, gateway)
+    except Exception as e:
+        print(e)
+    finally:
+        disconnect(gateway)
